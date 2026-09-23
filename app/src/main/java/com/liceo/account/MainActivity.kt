936b54878f5d30e1d66a9b3c9360f3cffe826347
Kurt7275 <kbautista99931@liceo.edu.ph>
2026-09-23 11:28:10 +0800

    Semi-final exam: login, create account, profile, screenshots

--- app/src/main/java/com/liceo/account/MainActivity.kt
+++ app/src/main/java/com/liceo/account/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.MaterialTheme
 import com.liceo.account.ui.LiceoAccountApp
+import com.liceo.account.ui.theme.LiceoAccountTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { MaterialTheme { LiceoAccountApp() } }
+        setContent { LiceoAccountTheme { LiceoAccountApp() } }
     }
 }
---
+++ app/src/main/java/com/liceo/account/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.liceo.account.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryLight = Color(0xFF800000)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val PrimaryContainerLight = Color(0xFF600000)
+val SecondaryLight = Color(0xFF800000)
+val SurfaceLight = Color(0xFFFFFFFF)
+val OnSurfaceVariantLight = Color(0xFF400000)
+
+val PrimaryDark = Color(0xFFFFB4AB)
+val OnPrimaryDark = Color(0xFF690005)
+val PrimaryContainerDark = Color(0xFF93000A)
+val SecondaryDark = Color(0xFFE7BDB8)
+val SurfaceDark = Color(0xFF1A1110)
+val OnSurfaceVariantDark = Color(0xFFD8C2C0)
